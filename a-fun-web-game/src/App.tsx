345b8742b956817b49a9790c000cc67173c4bee7
ed-amb <edenguy123@gmail.com>
2026-02-20 23:14:06 -0500

    Initial website base using template added svg

---
+++ a-fun-web-game/src/App.tsx
@@ -0,0 +1,27 @@
+
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <div>
+        
+      </div>
+      
+      <div className="card">
+        
+        <p>
+          
+        </p>
+      </div>
+      <p className="read-the-docs">
+        
+      </p>
+    </>
+  )
+}
+
+export default App
